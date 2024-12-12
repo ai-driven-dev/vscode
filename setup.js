2d2ea3a5ba463e1068f77f87a489aed0a36ffcdf
Alex <contact.alexsoyes@gmail.com>
2024-12-12 14:23:48 +0100

    feat: auto update imports on paste

--- setup.js
+++ setup.js
@@ -5,6 +5,7 @@ const path = require('path');
 // Chemins des fichiers
 const settingsPath = path.join(__dirname, 'settings.json');
 const packagePath = path.join(__dirname, 'package.json');
+const keybindingsPath = path.join(__dirname, 'keybindings.json');
 
 // Fonction pour lire le fichier JSON
 function readJsonFile(filePath) {
@@ -20,24 +21,31 @@ function writeJsonFile(filePath, data) {
 function updatePackageJson() {
     const settings = readJsonFile(settingsPath);
     const packageJson = readJsonFile(packagePath);
+    const keybindings = readJsonFile(keybindingsPath);
     const configDescriptions = {
         "workbench.editor.wrapTabs": "Wrap tabs for better visibility",
         "workbench.editor.enablePreview": "Disable preview mode for tabs",
         "explorer.compactFolders": "Disable compact folder display",
-        "css.hover.documentation": "Disable CSS hover documentation"
+        "css.hover.documentation": "Disable CSS hover documentation",
+        "editor.pasteAs.preferences": "Update imports on paste"
     };
 
     packageJson.contributes.configuration.properties = {
         ...packageJson.contributes.configuration.properties,
         ...Object.keys(settings).reduce((acc, key) => {
             acc[key] = {
-                type: 'boolean',
+                type: Array.isArray(settings[key]) ? 'array' : 'boolean',
                 default: settings[key],
                 description: configDescriptions[key] || ``
             };
             return acc;
         }, {})
     };
+
+    packageJson.contributes.keybindings = [
+        ...packageJson.contributes.keybindings || [],
+        keybindings
+    ];
 
     writeJsonFile(packagePath, packageJson);
 }
